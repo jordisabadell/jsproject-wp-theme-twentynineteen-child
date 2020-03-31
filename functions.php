7c2f1fe8ee538ccc738dff1f67b863e9e945a9e4
Jordi Sabadell <jordisabadell@gmail.com>
2020-03-31 16:20:36 +0200

    Add theme parent

--- functions.php
+++ functions.php
@@ -1,3 +1,7 @@
 <?php 
-
+//add theme parent
+function childtheme_parent_styles() {    
+    wp_enqueue_style( 'parent', get_template_directory_uri().'/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'childtheme_parent_styles');
 ?>
