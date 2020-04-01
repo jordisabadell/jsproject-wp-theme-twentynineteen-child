d84d2c8735ca14afcf368d0aa6711a1672f539bb
Jordi Sabadell <jordisabadell@gmail.com>
2020-04-01 06:51:07 +0200

    Add Sass

--- functions.php
+++ functions.php
@@ -1,7 +1,15 @@
-<?php 
-//add theme parent
-function childtheme_parent_styles() {    
-    wp_enqueue_style( 'parent', get_template_directory_uri().'/style.css' );
+<?php
+ 
+function my_theme_enqueue_styles() {
+  
+    $parent_style_handle = 'twentynineteen-style'; // change this value
+  
+    wp_enqueue_style( $parent_style_handle, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'twentynineteen-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style_handle )
+    );
 }
-add_action( 'wp_enqueue_scripts', 'childtheme_parent_styles');
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+ 
 ?>
